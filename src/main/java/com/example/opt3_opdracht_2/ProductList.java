933bb977f3e853a7932072b95d6908fe15eaa478
Dylan <drampersad740@gmail.com>
2022-06-23 00:47:43 +0200

    comments over javafx probleem toegevoegd ter verduidelijking.

--- src/main/java/com/example/opt3_opdracht_2/ProductList.java
+++ src/main/java/com/example/opt3_opdracht_2/ProductList.java
@@ -24,10 +24,13 @@ public class ProductList extends Observable{
         return ProductList;
     }
 
+    //Probleem met javaFX: Wanneer het meegegeven product zich in de index van de tableview bevindt, en dit vervolgens product op dezelfde index wordt vervangen
+    // Doormiddel van remove en add, dan wilt de tableview zich niet updaten.
+    // Doormiddel van set, in dit geval, wilt de tableview zich wel updaten.
     public void setProduct(Product product){
-        ProductList.remove(product);
-        ProductList.add(product);
-        System.out.println(product.getStatus());
+        ProductList.remove(product); //als de product zich in index 0 bevindt en delete
+        ProductList.add(0, product); //en het weer op dezelfde index 0 wordt toegevoegd, update de tableview niet.
+        ProductList.set(0, product); //tableview updated succesvol.
         notifyObservers();
     }
 
